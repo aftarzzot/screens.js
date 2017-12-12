f95feba2a17af5aefd6b1fa3e1f11d2f2893be4f
Yurkin Alexandr <aftarzzot@mail.ru>
2017-12-12 15:39:21 +0300

    ---

--- www/js/check_date.js
+++ www/js/check_date.js
@@ -1,4 +1,3 @@
-
 function checkDateEntrance(date, period) {
 	var periods = period.split(',');
 	return (date >= new Date(periods[0]) && date <= new Date(periods[1]));
@@ -7,4 +6,3 @@ if (checkDateEntrance(new Date(), '2017-08-01,2017-09-01')) {
 
 	alert('true!');
 }
-
